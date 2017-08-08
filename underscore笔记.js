e040eb165f0ddda637a8b63271a33812e47b6901
dongliang1993 <809812556@qq.com>
2017-08-08 17:10:50 +0800

    object

--- underscore笔记.js
+++ underscore笔记.js
@@ -220,3 +220,19 @@ _.invert = function(obj) {
   }
   return result
 }
+
+// Converts lists into objects. Pass either a single array of `[key, value]`
+// pairs, or two parallel arrays of the same length -- one of keys, and one of
+// the corresponding values.
+// 将数组转化为对象
+_.object = function(list, values) {
+  const result = {}
+  for (let i = 0, length = getLength(list); i < length; i++) {
+    if (values) {
+      result[list[i]] = values[i]
+    } else {
+      result[list[i][0]] = list[i][1]
+    }
+  }
+  return result
+}
